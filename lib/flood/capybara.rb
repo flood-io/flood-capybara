1673d1367b34db3b3cb1d957288f622b2100f37d
Tim Koopmans <tim.koops@gmail.com>
2014-11-15 15:35:24 +0800

    building new gem

---
+++ lib/flood/capybara.rb
@@ -0,0 +1,7 @@
+require "flood/capybara/version"
+
+module Flood
+  module Capybara
+    # Your code goes here...
+  end
+end
---
+++ lib/flood/capybara/version.rb
@@ -0,0 +1,5 @@
+module Flood
+  module Capybara
+    VERSION = "0.0.1"
+  end
+end
