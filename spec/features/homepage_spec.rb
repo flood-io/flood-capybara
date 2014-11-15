9a67ac74082372ec879e40cbf92a9a91f367dd7c
Tim Koopmans <tim.koops@gmail.com>
2014-11-16 07:38:11 +0800

    fix some shit up

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,4 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+require 'cgi'
+
+ENV['PUBLIC_IPV4'] ||= `curl -s --fail --connect-timeout 1 http://169.254.169.254/latest/meta-data/public-ipv4 || curl -s --connect-timeout 10 ifconfig.me || echo 127.0.0.1`.chomp
 
 describe "sign in", type: :feature do
   before :each do
@@ -9,17 +12,18 @@ describe "sign in", type: :feature do
     Capybara.default_driver == :poltergeist &&
       page.driver.network_traffic.each do |request|
         @client.index index: "results-#{Time.now.utc.strftime("%Y.%m.%d")}", type: 'capybara', body: {
-          timestamp: (Time.now.utc.to_f * 1000).to_i,
+          timestamp: (Time.now.utc.to_f * 1000).to_i.to_s,
           url: request.url,
+          label:  CGI::escape(request.url),
           request_headers: request.headers.to_s,
           response_headers: request.response_parts.last.headers.to_s,
-          start_time: (request.time.to_f * 1000).to_i,
-          end_time: (request.response_parts.last.time.to_f * 1000).to_i,
-          source_host: 'localhost',
+          start_time: (request.time.to_f * 1000).to_i.to_s,
+          end_time: (request.response_parts.last.time.to_f * 1000).to_i.to_s,
+          source_host: ENV['PUBLIC_IPV4'],
           response_time: (request.response_parts.last.time.to_f * 1000).to_i - (request.time.to_f * 1000).to_i,
           latency: nil,
           sample_count: 1,
-          thread_id: 1,
+          thread_id:  ENV['THREAD_ID'] || 1,
           active_threads: 1,
           active_threads_in_group: 1,
           uuid: ENV['FLOOD_UUID'],
@@ -31,6 +35,13 @@ describe "sign in", type: :feature do
       end
   end
 
+  it "should just work" do
+    visit '/'
+    expect(page).to have_content 'Flood'
+  end
+end
+
+describe "navigate", type: :feature do
   it "should visit homepage" do
     visit '/'
     expect(page).to have_content 'Flood'
@@ -38,6 +49,21 @@ describe "sign in", type: :feature do
 
   it "should visit features page" do
     visit '/features'
-    expect(page).to have_content 'features'
+    expect(page).to have_content 'Features'
+  end
+
+  it "should visit pricing page" do
+    visit '/pricing'
+    expect(page).to have_content 'Pricing'
+  end
+
+  it "should visit faq page" do
+    visit '/faq'
+    expect(page).to have_content 'Frequently Asked Questions'
+  end
+
+  it "should visit broken page" do
+    visit '/broken'
+    expect(page).to have_content '404'
   end
 end
